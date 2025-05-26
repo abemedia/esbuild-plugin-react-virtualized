1fb4c578b0df171f7fc19c964e12eb2e975ca445
Adam Bouqdib <adam@abemedia.co.uk>
2025-05-26 10:35:33 +0100

    fix: update regex for broken import in react-virtualized plugin

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { readFile } from 'node:fs/promises'
 import type { Plugin } from 'esbuild'
 
 const filter = /react-virtualized[/\\]dist[/\\]es[/\\]WindowScroller[/\\]utils[/\\]onScroll\.js$/
+const broken = /import { bpfrpt_proptype_WindowScroller } from ['"]\.\.\/WindowScroller\.js['"];/
 
 const reactVirtualized: Plugin = {
   name: 'esbuild-plugin-react-virtualized',
   setup({ onLoad }) {
     onLoad({ filter }, async ({ path }) => {
       const code = await readFile(path, 'utf8')
-      const broken = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`
       return { contents: code.replace(broken, '') }
     })
   },
